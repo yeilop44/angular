01fc5949e93166ae225dd24f34ed18e5ea906718
yeilop44 <pc_ylopez@hotmail.com>
2018-09-05 10:41:57 -0500

    commit

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', (req, res)=> {
+	res.res.sendFile(path.join(__dirname +'/dist/index.html'));
+})
